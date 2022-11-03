74f41f91ee660040fd54e917c1ae167e48089c7f
kayongo <kayonbrian@gmail.com>
2022-11-03 10:54:45 +0200

    added a few language words for Lusoga

---
+++ Lusoga.py
@@ -0,0 +1,12 @@
+lusoga = {
+    'Good morning': 'Wasuze otya',
+    'Good afternoon/evening': 'Osibye otya',
+    'Sir': 'Ssebo',
+    'Madam': 'Nyabo',
+    'Good': 'Bulungi',
+    'welcome': 'Tusangaire',
+    'welcome one': 'Nsangaire',
+    'Alright': 'Kale',
+    'glad to see you': 'Tusangaire okukubona',
+    'Please have a seat': 'Akatebe kako'
+}
