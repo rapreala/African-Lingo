b6e3a198c6ac2a9da0ca1bbb49c21ee18e2dde72
kayongo <kayonbrian@gmail.com>
2022-11-20 20:01:55 +0200

    new support

--- swahili.py
+++ swahili.py
@@ -3,5 +3,7 @@ from googletrans import Translator
 translator = Translator()
 
 word = str(input("Enter an English word to translate to swahili: "))
-result = translator.translate(text=word, dest='lg')
+
+result = translator.translate(text=word, dest='sw')
+
 print(f"{word} in Swahili is: {result.text}")
