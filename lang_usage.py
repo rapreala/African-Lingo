45a5e169e6577145c2906184451a51e73c212ddc
kayongo <kayonbrian@gmail.com>
2022-11-21 22:04:37 +0200

    changed the package to a single file

---
+++ lang_usage.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'''
+    This is the file that takes in all the modules
+    and functions that will translate and run the languages
+''' 
+from googletrans import Translator
+translator = Translator()
+context = {
+    '1' : 'af',
+    '2' : 'am',
+    '3' : 'ny',
+    '4' : 'kr' ,
+    '5' : 'ha',
+    '6' : 'ig',
+    '7' : 'mg',
+    '8' : 'st', 
+    '9' : 'sn', 
+    '10' : 'so',
+    '11' : 'sw', 
+    '12' : 'xh',
+    '13' : 'yo', 
+    '14' : 'zu',
+}
+
+def translate_lang(word,language):
+    ''' runs the modules and functions that run the langs '''
+    lang = context[str(language)]
+    result = translator.translate(text=word, dest=lang)
+    
+    return result
+
+
+
--- main.py
+++ main.py
@@ -1,12 +1,67 @@
-print('''
+#!/usr/bin/python3
+'''The opening file for the application'''
+import json
+
+
+translate_lang = __import__('lang_usage').translate_lang
+
+
+lan_supported = {
+        1 : 'Afrikans',
+        2 : 'Amharic',
+        3 : 'Chichewa',
+        4 : 'Hangul',
+        5 : 'Hausa',
+        6 : 'Igbo', 
+        7 : 'Malagasy',
+        8 : 'Sesotho', 
+        9 : 'Shona', 
+        10 : 'Somali',
+        11 : 'Swahili', 
+        12 : 'xhosa',
+        13 : 'Yoruba', 
+        14 : 'Zulu',
+    }
+
+print(
+    '''
     Hello, Welcome to African-Lingo
-    we Have Created a Data set of many African Languages and we would love you
-    to help you translate your English Words to the the African Native Language \n\n''')
+    we Have Created a Data set of many African Languages and we love to help you
+    translate your English Words to the African Native Language of your choice  
+    '''
+)
 
 name = input("To get Started, Please Tell us your name: ")
 
-print(f'''
-    Welcome {name} 
-    In- order for us to go on, 
-    Please you are going choose a language you want to continue with  
-    ''')
+choice = int(input('''
+    Welcome {name}
+    Please Choose a language of your choice to get started.
+    
+    Please Make a choice to continue the program 
+    1 : To continue  
+    2 : To Exit() the program 
+    
+    Choose a number(1 / 2) : '''))
+
+
+if choice == 1:
+    lang_chose = int(input(f'''
+        In order to choose a language, Just Choose the language number (eg. 1)
+        {
+            json.dumps(lan_supported, indent=8, sort_keys=True)
+        }
+        
+        Please choose the language you want : '''))
+    lang = lan_supported[lang_chose]
+    word = str(input(f"Enter an English word to translate to {lang}: "))
+    result = translate_lang(word, lang_chose)
+    print(result.text)
+else:
+    print('''
+        We are sad to see you go {name}. 
+        Have a wonderful Day
+    '''  
+)
+
+
+
