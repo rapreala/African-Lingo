0ac353bb9f22c22a9f7b0485ef7403b2e117fdf0
kayongo <kayonbrian@gmail.com>
2022-11-22 10:34:33 +0200

    using recusive functions in the system

--- main.py
+++ main.py
@@ -7,22 +7,39 @@ translate_lang = __import__('lang_usage').translate_lang
 
 
 lan_supported = {
-        1 : 'Afrikans',
-        2 : 'Amharic',
-        3 : 'Chichewa',
-        4 : 'Hangul',
-        5 : 'Hausa',
-        6 : 'Igbo', 
-        7 : 'Malagasy',
-        8 : 'Sesotho', 
-        9 : 'Shona', 
-        10 : 'Somali',
-        11 : 'Swahili', 
-        12 : 'xhosa',
-        13 : 'Yoruba', 
-        14 : 'Zulu',
+    1 : 'Afrikans',
+    2 : 'Amharic',
+    3 : 'Chichewa',
+    4 : 'Hangul',
+    5 : 'Hausa',
+    6 : 'Igbo', 
+    7 : 'Malagasy',
+    8 : 'Sesotho', 
+    9 : 'Shona', 
+    10 : 'Somali',
+    11 : 'Swahili', 
+    12 : 'xhosa',
+    13 : 'Yoruba', 
+    14 : 'Zulu',
+    15 : 'Exit'
 }
 
+
+def keep_translating(lang_chose, exitcode):
+    if exitcode == 0:
+        return 0
+    else:
+        lang = lan_supported[lang_chose]
+        word = str(input(f"Enter an English word to translate to {lang}: "))
+        result = translate_lang(word, lang_chose)
+        print(f"""
+              '{word}' in '{lang}' is: {result.text} \n
+        """)
+        exitcode =  int(input(
+            """
+            Press 0 exit 
+            Press 1 to use another word : """))
+        return keep_translating(lang_chose, exitcode)
 
 
 print(
@@ -35,6 +52,9 @@ print(
 
 name = input("To get Started, Please Tell us your name: ")
 
+
+
+
 choice = int(input(f'''
     Welcome {name}
     Please Choose a language of your choice to get started.
@@ -45,8 +65,7 @@ choice = int(input(f'''
     
     Choose a number(1 / 2) : '''))
 
-
-if choice == 1:
+def Main():
     lang_chose = int(input(f'''
         In order to choose a language, Just Choose the language number (eg. 1)
         {
@@ -55,18 +74,12 @@ if choice == 1:
         
         Please choose the language you want : '''))
     lang = lan_supported[lang_chose]
-    word = str(input(f"Enter an English word to translate to {lang}: "))
-    
-    
-    cont = int(input("Press 1 to continue or 0 to exit()"))
-    result = translate_lang(word, lang_chose)
-    print(result.text)
-else:
-    print(f'''
-        We are sad to see you go {name}. 
-        Have a wonderful Day
-    '''  
-    )
+    if lang_chose == 15:
+        return 0
+    else:
+        result = keep_translating(lang_chose, 1)
+        if result == 0:
+            return Main()
 
 
-
+Main()
